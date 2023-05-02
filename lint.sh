0c4c45e778e9d53934b054566b7f4ff7120b78d3
Gulshan Singh <gsingh2011@gmail.com>
2023-05-02 00:18:38 -0700

    Fix typesafety tests

--- lint.sh
+++ lint.sh
@@ -10,4 +10,8 @@ isort --check-only --diff ${LINT_FILES}
 black --check --diff ${LINT_FILES}
 flake8 --show-source ${LINT_FILES}
 # mypy ${LINT_FILES} || true
-pyright ${LINT_FILES} || true
+
+# We use tests/*.py here to exclude the typesafety directory. We can't just
+# exclude this directory in the config because then it won't be checked at all
+# by pytest-pyright
+pyright src/pyanimate examples tests/*.py
