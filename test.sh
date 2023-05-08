3baf3a672a2ac45eea3cc953468ea83720e6ab8e
Gulshan Singh <gsingh2011@gmail.com>
2023-05-07 22:48:22 -0700

    Use pytest-cov for coverage

--- test.sh
+++ test.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
+
+export PYTEST_ADDOPTS="--showlocals -v"
+
 FILTER=${1:-""}
 if [ "$FILTER" != "" ]; then
+    PYTEST_ADDOPTS="-k $FILTER $PYTEST_ADDOPTS"
     shift
 fi
 
-pytest -v -l -k "$FILTER" --pyright-dir tests/typesafety tests $@
+if [[ $CI == true ]]; then
+    PYTEST_ADDOPTS="--cov $PYTEST_ADDOPTS"
+fi
+pytest --pyright-dir tests/typesafety tests $@
