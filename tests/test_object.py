11e1ba2d4692a31b1a2c8b60249c9e36f260f8cb
Gulshan Singh <gsingh2011@gmail.com>
2023-05-27 11:49:46 -0700

    Improved Object tests

--- tests/test_object.py
+++ tests/test_object.py
@@ -1,47 +1,79 @@
 import pytest
-from kiwisolver import UnsatisfiableConstraint
 
-from pyanimate.layout import Object, VLayout
+from pyanimate.layout import Object
 from pyanimate.shape import RED
 from pyanimate.shape import Point as P
 from pyanimate.style import Style
 
 
 class TestObject:
-    def test_add(self, c, mock_renderer) -> None:
+    def test_width(self, c, mock_renderer) -> None:
+        """
+        Test that width is properly set
+        """
+        obj = Object(c, width=10)
+        obj.prepare(mock_renderer)
+        assert obj.width.value() == 10
+
+        obj = Object(c, width=10)
+        obj.width = 20
+        obj.prepare(mock_renderer)
+        assert obj.width.value() == 20
+
+    @pytest.mark.xfail
+    def test_width_constraint(self, _c, _mock_renderer):
+        assert False
+
+    def test_height(self, c, mock_renderer) -> None:
+        """
+        Test that height is properly set
+        """
+        obj = Object(c, height=10)
+        obj.prepare(mock_renderer)
+        assert obj.height.value() == 10
+
+        obj = Object(c, height=10)
+        obj.height = 20
+        obj.prepare(mock_renderer)
+        assert obj.height.value() == 20
+
+    @pytest.mark.xfail
+    def test_height_constraint(self, _c, _mock_renderer):
+        assert False
+
+
+class TestParentChildObject:
+    @pytest.fixture
+    def child(self, c) -> Object:
+        return Object(c)
+
+    @pytest.fixture
+    def parent(self, c, child, mock_renderer) -> Object:
+        parent = Object(c)
+
+        parent.add(child)
+        parent.prepare(mock_renderer)
+
+        return parent
+
+    def test_add(self, parent, child) -> None:
         """
         Test that a child object is properly added to the parent object
         """
-        parent = Object(c)
-        child = Object(c)
-        parent.add(child)
-
-        parent.prepare(mock_renderer)
-
         assert child in parent.children
         assert parent.children[child] == P(0, 0)
         assert child.parent == parent
         assert child.style.parent_obj_style == parent.style
         assert child.pos == P(0, 0)
 
-    def test_add_twice(self, c) -> None:
+    def test_add_twice(self, parent, child) -> None:
         """
         Test that adding a child object twice raises an error
         """
-        parent = Object(c)
-        child = Object(c)
-        parent.add(child)
-
         with pytest.raises(AssertionError):
             parent.add(child)
 
-    def test_remove(self, c, mock_renderer) -> None:
-        parent = Object(c)
-        child = Object(c)
-        parent.add(child)
-
-        parent.prepare(mock_renderer)
-
+    def test_remove(self, parent, child, mock_renderer) -> None:
         assert child in parent.children
         assert parent.children[child] == P(0, 0)
         assert child.parent == parent
@@ -49,7 +81,7 @@ class TestObject:
 
         parent.remove(child)
 
-        assert child not in c.children
+        assert child not in parent.children
         assert child.parent is None
 
         parent.add(child, P(10, 20))
@@ -62,41 +94,33 @@ class TestObject:
         assert child.pos == P(10, 20)
 
     @pytest.mark.xfail
-    def test_replace(self, c) -> None:
+    def test_replace(self, _parent, _child) -> None:
         assert False
 
-    def test_width(self, c, mock_renderer) -> None:
-        obj = Object(c, width=10)
-        obj.prepare(mock_renderer)
-        assert obj.width.value() == 10
 
-        obj = Object(c, width=10)
-        obj.width = 20
-        obj.prepare(mock_renderer)
-        assert obj.width.value() == 20
+class TestLatestObject:
+    @pytest.fixture
+    def parent(self, c) -> Object:
+        return Object(c)
 
-        # TODO: Test setting width to constraint
+    @pytest.fixture
+    def cloned(self, parent) -> Object:
+        return parent.clone()
 
-    def test_height(self, c, mock_renderer) -> None:
-        obj = Object(c, height=10)
-        obj.prepare(mock_renderer)
-        assert obj.height.value() == 10
-
-        obj = Object(c, height=10)
-        obj.height = 20
-        obj.prepare(mock_renderer)
-        assert obj.height.value() == 20
-
-    def test_latest(self, c) -> None:
+    @pytest.mark.dependency(name="latest_is_self")
+    def test_latest_is_self(self, parent) -> None:
         """
-        Test that latest() returns the latest object
+        Test that latest() returns self before any clones
         """
-        parent = Object(c)
-
         assert parent.latest() == parent
         assert parent.latest() is parent
 
-        cloned1 = parent.clone()
+    @pytest.mark.dependency(name="latest", depends=["latest_is_self"])
+    def test_latest(self, parent, cloned) -> None:
+        """
+        Test that latest() returns the latest object
+        """
+        cloned1 = cloned
 
         assert parent.latest() == cloned1
         assert cloned1.latest() == cloned1
@@ -104,6 +128,19 @@ class TestObject:
         assert parent.latest() is cloned1
         assert cloned1.latest() is cloned1
 
+        cloned2 = cloned1.clone()
+
+        assert parent.latest() == cloned2
+        assert cloned1.latest() == cloned2
+        assert cloned2.latest() == cloned2
+
+        assert parent.latest() is cloned2
+        assert cloned1.latest() is cloned2
+        assert cloned2.latest() is cloned2
+
+    @pytest.mark.dependency(depends=["latest"])
+    def test_diverging_clone(self, parent, cloned) -> None:
+        cloned1 = cloned
         cloned2 = parent.clone()
 
         assert parent.latest() == cloned2
@@ -116,6 +153,8 @@ class TestObject:
         # TODO: Is this what we want?
         assert cloned1.latest() is not cloned2
 
+
+class TestCloneObject:
     def test_clone(self, c, mock_renderer) -> None:
         """
         Test that cloning an object replicates all attributes
@@ -175,14 +214,14 @@ class TestObject:
         assert cloned.latest() is cloned
 
 
-class TestConstraints:
-    @pytest.mark.xfail
-    def test_unsatisfiable(self, c) -> None:
-        """
-        Test that unsatisfiable constraints raise an error
-        """
-        vlayout = VLayout(canvas=c, width=10)
-        obj = Object(c, width=20)
+# class TestConstraints:
+#     @pytest.mark.xfail
+#     def test_unsatisfiable(self, c) -> None:
+#         """
+#         Test that unsatisfiable constraints raise an error
+#         """
+#         vlayout = VLayout(canvas=c, width=10)
+#         obj = Object(c, width=20)
 
-        with pytest.raises(UnsatisfiableConstraint):
-            vlayout.add(obj)
+#         with pytest.raises(UnsatisfiableConstraint):
+#             vlayout.add(obj)
