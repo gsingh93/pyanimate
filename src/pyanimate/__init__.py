394869295e83a3ae70c17c9c60283ae2b92592e0
Gulshan Singh <gsingh2011@gmail.com>
2023-05-29 23:01:52 -0700

    Make logger indentation more accurate

--- src/pyanimate/__init__.py
+++ src/pyanimate/__init__.py
@@ -16,18 +16,23 @@ logging.VERBOSE = VERBOSE  # type: ignore[attr-defined]
 class IndentFormatter(logging.Formatter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print("stack depth: ", len(inspect.stack()))
-        # self.base_depth = len(inspect.stack())
-        self.base_depth = 0
+        self.base_depth = None
 
     def format(self, rec: logging.LogRecord) -> str:
         stack: list[inspect.FrameInfo] = inspect.stack()
+
         # We need to skip 8 stack frames of the logging infrastructure to get to the
         # real stack frame
         frame = stack[8]
 
+        # We assume that the first time we call the logger we are at the base depth
+        # TODO: Is there a more robust way to do this?
+        if self.base_depth is None:
+            # We want the indent to be 1 for the first call, so if `base_depth` is set
+            # as below, then the indent is `len(stack) - 8 - (len(stack) - 8 - 1) = 1`
+            self.base_depth = len(stack) - 8 - 1
+
         rec.indent = " " * (len(stack) - 8 - self.base_depth)
-        # TODO: Also need to update filename
         rec.funcName = frame.function
         out = logging.Formatter.format(self, rec)
         first, *rest = out.split("]")
@@ -99,7 +104,7 @@ def get_logger(name: str, *, indent: bool = False) -> CustomLogger:
     # still allow the use of the IndentFormatter.
     if indent:
         formatter = IndentFormatter(
-            "[{levelname:7} - {filename}:{lineno}]{indent}{funcName}(): {message}",
+            "[{levelname:7} - {filename}:{lineno}] {indent}{funcName}(): {message}",
             style="{",
         )
     else:
